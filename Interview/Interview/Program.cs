19ac2d2617997c3cf7e6beb4b2476f4b73de0ebf
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2024-10-14 21:07:28 +0400

    Task12

---
+++ Interview/Interview/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Interview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+                1.Konstruktor(Constructor) nədir ? 
+                2.Virtual metodlar(Virtual Methods) nədir ?
+                3.OOP nədir ?
+                4.Kapsülleme(Encapsulation) nədir
+                5.Kalıtım(Inheritance) nədir ?
+                6.Soyutlama(Abstraction) nədir ?
+                7.Tuple nedir?
+                8.Kodlar nece isleyir?
+                9.Access modifile haqqda
+                10.Recort nedir?
+            */
+
+        }
+    }
+}
